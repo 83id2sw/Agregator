624c63e5c20d431383c76c9e65587e9949b8caf4
damd1 <damd@yandex.ru>
2023-03-06 21:23:34 +0300

    Prosto Sir

---
+++ major/src/main/java/com/app/shop/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.app.shop.repository;
+
+import com.app.shop.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+}
