f1099144628e18551724a7af502ba1fe8fd6d2c8
damd1 <damd@yandex.ru>
2023-05-22 02:01:27 +0300

    Price-conversion

--- major/src/main/resources/static/js/price-conversion.js
+++ major/src/main/resources/static/js/price-conversion.js
@@ -1,9 +1,7 @@
 window.onload = function () {
     let prices = document.getElementsByClassName('item-price');
-    console.log(prices)
     for (let i = 0; i < prices.length; i++) {
-        console.log(prices[i].textContent)
-        prices[i].innerHTML = prices[i].textContent + ' ≈ ₽' + Math.round(prices[i].textContent.slice(1) * 79.98)
+        prices[i].innerHTML = prices[i].textContent + ' ≈ ₽' + Math.round(prices[i].textContent.slice(1) * 79.91)
     }
 }
 
