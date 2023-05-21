eb472ec644f34541d531af62d06a049c70c3961d
Dmitryq <thepogorny@gmail.com>
2023-05-22 02:06:26 +0300

    dwe

--- major/src/main/resources/static/js/price-conversion.js
+++ major/src/main/resources/static/js/price-conversion.js
@@ -1,10 +1,15 @@
 window.onload = function () {
     let prices = document.getElementsByClassName('item-price');
+    let categories = document.getElementsByClassName('category-name');
+    let sex = document.getElementsByClassName('SEX');
+    
     console.log(prices)
     for (let i = 0; i < prices.length; i++) {
         console.log(prices[i].textContent)
         prices[i].innerHTML = prices[i].textContent + ' ≈ ₽' + Math.round(prices[i].textContent.slice(1) * 79.98)
     }
+ 
+
 }
 
 
